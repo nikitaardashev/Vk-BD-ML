aa36d1af4facfdb527d40357e9c8662ea469c0e6
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2021-01-23 18:19:31 +0500

    Make predictor work on another machine

--- model/predictor.py
+++ model/predictor.py
@@ -1,10 +1,14 @@
 import os
-from tensorflow.keras.models import load_model
+from tensorflow.keras import Sequential, layers, models, losses, metrics
+from tensorflow.keras.layers.experimental.preprocessing import (
+        TextVectorization)
 
 
 class Predictor:
     def __init__(self, model_path):
-        self.model = load_model(model_path)
+        self.model = None
+        self.load_model(model_path)
+
         self.class_names = []
         with open(os.path.join(model_path, 'class_names.txt'), 'r') as f:
             for line in f.readlines():
@@ -17,7 +21,30 @@ class Predictor:
         return [self.class_names[list(pr).index(max(pr))]
                 for pr in self.model.predict(text_list)]
 
+    def load_model(self, model_path):
+        ckpt = os.path.join(model_path, 'checkpoint')
+        if os.path.isfile(ckpt):
+            vectorize_layer = TextVectorization(
+                max_tokens=20000,
+                output_mode='int',
+                output_sequence_length=40)
+
+            # ATTENTION: this model MUST be absolutely
+            # identical to the original one
+            self.model = Sequential([vectorize_layer, Sequential([
+                layers.Embedding(20001, 160),
+                layers.Dropout(0.3),
+                layers.GlobalAveragePooling1D(),
+                layers.Dropout(0.3),
+                layers.Dense(40)])])
+            self.model.load_weights('weights/checkpoint')
+        else:
+            self.model = models.load_model(model_path)
+
 
 if __name__ == '__main__':
-    p = Predictor('export_model')
-    print(p.predict('олег'))
+    p1 = Predictor('weights')
+    print(p1.predict(['лингвистика', 'олег', 'физика', 'программирование']))
+
+    p2 = Predictor('export_model')
+    print(p2.predict(['лингвистика', 'олег', 'физика', 'программирование']))
