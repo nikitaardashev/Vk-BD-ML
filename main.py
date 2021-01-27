b8445e3e225bf63a8cc4e2a5d8587ac4d412b955
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2021-01-27 22:52:53 +0500

    Error in predictor?

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from web.Bot import Bot
-# from database import db_session
+from web import Bot
+from database import db_session
 
 
 if __name__ == '__main__':
-    # db_session.global_init('database/db.sqlite')
-    #
-    # bot = Bot()
-    # bot.listen()
+    db_session.global_init('database/db.sqlite')
 
-    print("Test logging")
+    bot = Bot()
+    bot.listen()
