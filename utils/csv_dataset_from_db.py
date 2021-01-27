f1e4d1db5d76c891b6a54e3be5106e99bd8b6fdb
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2021-01-27 18:33:40 +0500

    Prepare for heroku and some fixes

--- utils/csv_dataset_from_db.py
+++ utils/csv_dataset_from_db.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import vk_api
 
@@ -6,10 +7,10 @@ from database.models.GroupsIds import GroupsIds
 from utils.cleaner import Cleaner
 
 
-def csv_dataset_from_db(post_count=1):
-    service_token = '5c0ff5695c0ff5695c0ff569145c7b34f055c0f5c0ff56903ac3c08de3d59cf69e6645b'  # сервисный ключ доступа (из приложения)
-    app_id = 7651737  # ID приложения
-    client_secret = 'yeRqPtdVKHU4bEJug1aX'  # защищённый ключ (из приложения)
+def csv_dataset_from_db(post_count=1, max_posts=None):
+    app_id = int(os.environ.get('APP_ID'))
+    service_token = os.environ.get('SERVICE_TOKEN')
+    client_secret = os.environ.get('CLIENT_SECRET')
     service_session = vk_api.VkApi(app_id=app_id, token=service_token,
                                    client_secret=client_secret)
     api = service_session.get_api()
@@ -25,16 +26,21 @@ def csv_dataset_from_db(post_count=1):
 
     with open('data/dataset.csv', 'w', encoding='utf-8') as f:
         csv_file = csv.writer(f, delimiter=',')
-        for group in session.query(GroupsIds).order_by(GroupsIds.group_id):
-            # if posts_loaded > 100:
-            #     break
+        groups = session.query(GroupsIds).order_by(GroupsIds.group_id)
+        total = groups.count()
+        for i, group in enumerate(groups):
+            if isinstance(max_posts, int) and posts_loaded >= max_posts:
+                break
             try:
                 posts = api.wall.get(owner_id=-int(group.group_id),
                                      count=post_count)
             except vk_api.exceptions.ApiError:
-                print(f'Access denied: wall {group.group_id} id disabled')
+                print(f'\rAccess denied: wall {group.group_id} id disabled')
+                continue
             else:
-                print(f'Posts from group {group.group_id} received')
+                print(f'\rPosts from group {group.group_id} received')
+                if not posts:
+                    continue
             for post in posts['items']:
                 text = cleaner.clean_text(post['text'])
                 if not post['marked_as_ads'] and text:
@@ -43,6 +49,8 @@ def csv_dataset_from_db(post_count=1):
                         class_names.index(group.subject.lower())
                     ])
                     posts_loaded += 1
+            print(f'\r[{("#"*int((i + 1) / total * 10)).ljust(10, " ")}] '
+                  f'{i + 1} of {total} ({posts_loaded} posts)', end='')
 
     with open('data/ds_info.txt', 'w', encoding='utf-8') as f:
         f.write(f"{','.join(class_names)}\n")
