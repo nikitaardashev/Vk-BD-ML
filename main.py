f351038bdcc51d0e4ff1a187bddcc18a21993699
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2021-01-27 22:04:06 +0500

    Test logging

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from database import db_session
 
 
 if __name__ == '__main__':
-    db_session.global_init('database/db.sqlite')
+    # db_session.global_init('database/db.sqlite')
+    #
+    # bot = Bot()
+    # bot.listen()
 
-    bot = Bot()
-    bot.listen()
+    print("Test logging")
