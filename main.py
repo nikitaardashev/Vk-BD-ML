052f5bdd591fbcebb78a3d817bb1ae9c7171ebed
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2021-02-04 18:10:33 +0500

    Add error report

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from database.db_session import DataBase
 
 if __name__ == '__main__':
     db = DataBase(os.environ.get('DATABASE_URL'))
+    log_dir = "https://dashboard.heroku.com/apps/vk-recommend/logs"
 
     bot = Bot(db)
-    bot.listen()
+    if os.environ.get('IS_DEPLOY'):
+        try:
+            bot.listen()
+        except Exception as e:
+            bot.send_message(159635403, f'Произошла ошибка:\n{e}\n\n{log_dir}')
+            bot.send_message(399923740, f'Произошла ошибка:\n{e}\n\n{log_dir}')
+            exit(1)
+    else:
+        bot.listen()
